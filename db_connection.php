fc78a4e35cc07ab8da1520359df8dc34adef1907
rt0w <s141129@student.squ.edu.om>
2025-05-14 13:14:23 +0400

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -4,10 +4,10 @@ $username = "root";
 $password = ""; 
 $dbname = "perfume_store"; 
 
-// محاولة الاتصال بقاعدة البيانات
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// التحقق من نجاح الاتصال
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error . " - " . $conn->connect_errno);
 } 
