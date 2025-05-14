c093b4dbb0a95307cfcafc16b1b85dfcbe4eeceb
rt0w <s141129@student.squ.edu.om>
2025-05-14 13:14:33 +0400

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -4,9 +4,7 @@ $username = "root";
 $password = ""; 
 $dbname = "perfume_store"; 
 
-
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error . " - " . $conn->connect_errno);
