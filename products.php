0477c055c38407d3f22c4a5fdaa72400ca4f39c7
rt0w <s141129@student.squ.edu.om>
2025-05-14 12:59:35 +0400

    Update products.php

--- products.php
+++ products.php
@@ -1,30 +1,30 @@
 <?php
-include 'db_connection.php';  // تضمين الاتصال بقاعدة البيانات
+include 'db_connection.php';  
 
-// استعلام لاستخراج جميع المنتجات
+
 $sql = "SELECT product_id, product_name FROM products";
 $result = $conn->query($sql);
 
-// التحقق من وجود نتائج
+
 if ($result === false) {
-    echo "خطأ في الاستعلام: " . $conn->error;
+    echo "query error " . $conn->error;
 } else {
     if ($result->num_rows > 0) {
         echo "<h2 class='text-center'>منتجاتنا</h2>";
         echo "<ul class='list-group'>";
         
-        // عرض جميع المنتجات
+   
         while ($row = $result->fetch_assoc()) {
-            // عرض رابط لعرض التقييمات الخاصة بكل منتج
+        
             echo "<li class='list-group-item'>";
-            // إضافة معرف المنتج (product_id) في الرابط
+           
             echo "<a href='display_reviews.php?product_id=" . $row["product_id"] . "'>" . $row["product_name"] . "</a>";
             echo "</li>";
         }
         
         echo "</ul>";
     } else {
-        echo "لا توجد منتجات لعرضها.";
+        echo "there are no products to reveiws";
     }
 }
 
